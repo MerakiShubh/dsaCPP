5de936594f2b032f2026a71dddd0b2cf37f43541
MerakiShubh <shubhamt.champ@gmail.com>
2025-09-28 01:04:05 +0530

    tabulation with memory optimization

--- fibonacciWithDP.cpp
+++ fibonacciWithDP.cpp
@@ -1,31 +1,62 @@
 #include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 const int N = 1e3;
-vector<long long> dp(N, -1);
+vector<int> dp(N, 0);
 
-long long fib(int n) {
-    if (n <= 1) return n;
 
-    if (dp[n] != -1) return dp[n];
+// Tob down ---> Space O(n) + O(n) with TC O(n)
+// int fib(int n){
+//     if(n<=1) return n;
+    
+//     if(dp[n]){
+//         return dp[n];
+//     }
+    
+//     dp[n] = fib(n - 1) + fib(n - 2);
+    
+//     return dp[n];
+// }
 
-    dp[n] = fib(n - 1) + fib(n - 2);
-    return dp[n];
+//Tabulation method ---> Space O(n) wtih TC O(n)
+// int fib(int n){
+//     dp[0] = 0;
+//     dp[1] = 1;
+    
+//     for(int i = 2; i<=n; i++){
+//         dp[i] = dp[i - 1] + dp[i - 2];
+//     }
+    
+//     return dp[n];
+// }
+
+//Space optimization ----> Space O(1) with TC O(n)
+
+int fib(int n){
+    int prev1 = 1;
+    int prev2 = 0;
+    
+    int curr;
+    for(int i = 2; i<=n; i++){
+        curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return prev1;
 }
 
 int main() {
     int n;
     cout << "Enter the number to calculate fib number: " << endl;
     cin >> n;
-
+    
     cout << fib(n) << endl;
-
+    
     cout << "dp: " << endl;
-    for (int i = 0; i <= n; i++) {
-        cout << "dp[" << i << "] = " << dp[i] << endl;
+    for(int i = 0; i<6; i++){
+        cout << dp[i] << " ";
     }
 
     return 0;
 }
-
