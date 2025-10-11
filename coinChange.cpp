f035ee345af8092b76aad897d923205c6e0d6555
MerakiShubh <shubhamt.champ@gmail.com>
2025-10-12 00:04:59 +0530

    add recursion with dp and tabultaion

--- coinChange.cpp
+++ coinChange.cpp
@@ -2,46 +2,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int solve3(vector<int> &v, int target){
-    vector<int>dp(target+1, INT_MAX);
+// int solve3(vector<int> &v, int target){
+//     vector<int>dp(target+1, INT_MAX);
 
-    dp[0] = 0;
+//     dp[0] = 0;
     
-    for(int i = 1; i<dp.size(); i++){
-        int mini = INT_MAX;
-        for(int j = 0; j<v.size(); j++){
-            if(i - v[j] >= 0 && dp[i - v[j]] != INT_MAX){
-                mini = min(mini, 1 + dp[i - v[j]]);
-            }
-        }
-        dp[i] = mini;
-    }
+//     for(int i = 1; i<dp.size(); i++){
+//         int mini = INT_MAX;
+//         for(int j = 0; j<v.size(); j++){
+//             if(i - v[j] >= 0 && dp[i - v[j]] != INT_MAX){
+//                 mini = min(mini, 1 + dp[i - v[j]]);
+//             }
+//         }
+//         dp[i] = mini;
+//     }
     
-   for(int i = 0; i<dp.size(); i++){
-       cout << i << " -> " << dp[i] << endl;
-   }
+//   for(int i = 0; i<dp.size(); i++){
+//       cout << i << " -> " << dp[i] << endl;
+//   }
     
-    return dp[target];
-}
+//     return dp[target];
+// }
 
-int solve2(vector<int> &v, int target, vector<int> &dp){
+// int solve2(vector<int> &v, int target, vector<int> &dp){
     
-      if(target == 0) return 0;
-      if(target < 0) return INT_MAX;
-      if(dp[target] != -1) return dp[target];
+//       if(target == 0) return 0;
+//       if(target < 0) return INT_MAX;
+//       if(dp[target] != -1) return dp[target];
       
-      int mini = INT_MAX;
-      for(int i = 0; i<v.size(); i++){
-          int ans = solve(v, target - v[i], dp);
-          if(ans != INT_MAX){
-              mini = min(mini, 1 + ans);
-          }
-      }
+//       int mini = INT_MAX;
+//       for(int i = 0; i<v.size(); i++){
+//           int ans = solve2(v, target - v[i], dp);
+//           if(ans != INT_MAX){
+//               mini = min(mini, 1 + ans);
+//           }
+//       }
     
-  return dp[target] = mini;
+//   return dp[target] = mini;
+// }
+
+
+int solve1(vector<int> &v, int target){
+     if(target == 0) return 0;
+     if(target < 0) return INT_MAX;
+     
+     int mini = INT_MAX;
+     for(int i = 0; i<v.size(); i++){
+         int ans = solve1(v, target - v[i]);
+         if(ans != INT_MAX){
+             mini = min(mini, 1 + ans);
+         }
+     }
+     
+     return mini;
 }
-
-
 
 
 int main() {
@@ -49,8 +63,7 @@ int main() {
   int target = 11;
   
   //method - 1 recursion
-  
-  cout << solve(v, target) << endl;
+  cout << solve1(v, target) << endl;
   
   
 //   method 2 top down (recursion + memoization) 
@@ -64,7 +77,7 @@ int main() {
 
 // method - 3 bottom up (tabulation) 
 
-cout << "ans: " << solve3(v, target) << endl;
+// cout << "ans: " << solve3(v, target) << endl;
 
 
 
